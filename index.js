e1d0bc5b657f7ee4ad0058f31e850d8b1a96548d
vinod <vinodkumarmahato1@gmail.com>
2023-09-27 08:08:46 +0530

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 document.querySelector("nav .fas").addEventListener("click", function () {
   const togglemenu = document.querySelector(".nav-links");
-  togglemenu.style.right = "0px";
-  togglemenu.style.display = "block";
-  togglemenu.style.transition = "all 0.5s ease";
+  if (!togglemenu.classList.contains("opend")) {
+    togglemenu.classList.add("opened");
+  }
 });
 
 document.querySelector(".nav-links i").addEventListener("click", function () {
   const closemenu = document.querySelector(".nav-links");
-  closemenu.style.right = "-200px";
-  closemenu.style.transition = "all 0.5s ease";
-  if ((closemenu.style.right = "-150px")) {
-    closemenu.style.display = "none";
-  } else {
-    closemenu.style.display = "block";
-    closemenu.style.transition = "all 0.5s ease";
+  if (!closemenu.classList.contains("opend")) {
+    closemenu.classList.remove("opened");
   }
 });
