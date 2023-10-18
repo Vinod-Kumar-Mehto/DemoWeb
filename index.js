353fa086a99cb3ecbaadbb3758a0221f8d0d32f3
vinod <vinodkumarmahato1@gmail.com>
2023-10-18 09:05:09 +0530

    first commit

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ document.querySelector(".nav-links i").addEventListener("click", function () {
     closemenu.classList.remove("opened");
   }
 });
+
+document.querySelector("nav img").addEventListener("click", () => {
+  document.location.href = "index.html";
+});
+
+window.onscroll = () => {
+  if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+    document.querySelector("nav").style.background = "rgb(170, 170, 170)";
+    document.querySelector("nav").style.transition = "all 0.5s";
+  } else {
+    document.querySelector("nav").style.background = "";
+  }
+};
